0ecd8afd8bc1dd0edbfc116fcf863bdc78e582c7
Vex1cK <118670080+Vex1cK@users.noreply.github.com>
2024-04-22 16:54:43 +0300

    aasjdfhlgkjfadng

--- spandas/preprocessing.py
+++ spandas/preprocessing.py
@@ -1,3 +1,6 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
 # write your code here
 # it is preferable to use Classes in this module
 # also use type annotations to make code more clear and efficient to write
