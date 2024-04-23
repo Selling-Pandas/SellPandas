70baa3ef5ef7610e2d1cc0307aa66e93f2e7766d
Vex1cK <loiiika2281337@gmail.com>
2024-04-23 16:15:58 +0300

    v 1.0 | возможны баги

--- spandas/preprocessing.py
+++ spandas/preprocessing.py
@@ -1,26 +1,29 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from dataclasses import dataclass
 from art import tprint
 
 # write your code here
 # it is preferable to use Classes in this module
 # also use type annotations to make code more clear and efficient to write
 
-
+@dataclass
 class Loggs:
+    """ Класс отвечает за вывод логов
+    Объект класса буквально такой: тут логи пиши, а тут логи не пиши
+    Такой объект передаётся в качестве параметра в основную функцию data_preprocessing"""
     test: bool = False
+    cols_selection: bool = True
+    was_became: bool = True
 
 
-def get_dict_of_cols(
-    df: pd.DataFrame, n=2, exclude=["id"], method: int = 1, ignore_strings: bool = False
+def get_list_of_cols(
+    df: pd.DataFrame, n=2, exclude=["id"], method: int = 1, ignore_strings: bool = True
 ):
     """
-    Returns a dict like:
-    {column: [str_value, str_value]}
-    where the str_value is one of the few string values in the column
-
-    if method == 2:
-        return[column] == []
+    Returns a list like:
+    [column, column, column ...]
+    where the column is name of selected column
 
     Parameters
     ----------
@@ -33,60 +36,61 @@ def get_dict_of_cols(
         The columns you want to exclude from the result
     method : int in (1, 2), default 1
         The method of selecting the columns
-    ignore_strings : bool, default False
-        Flag for ignoring columns with strings in them
+    ignore_strings : bool, default True
+        Flag for ignoring strings in columns
     """
     if method not in (1, 2):
         raise ValueError("Method must be 1 or 2")
 
-    result = {"method": method}
+    result = []
     if method == 1:
-        columns_in_df = df.columns
-        for column in columns_in_df:
-            if column in exclude:
+        for col in df.columns:
+            if col in exclude:
                 continue
-            str_count, strs, digit_count = 0, [], 0
-            values = df[column].value_counts().keys()
-            for val in values:
-                if str(val).isdigit():
-                    digit_count += 1
-                else:
-                    str_count += 1
-                    strs.append(val)
-                    if not ignore_strings:
-                        continue
-                if str_count > n:
-                    continue
-            if digit_count == 0:
+            if df[col].dtype not in ['float64', 'int64'] and not ignore_strings:
                 continue
-            result[column] = strs
+            if df[col].dtype in ['float64', 'int64']:
+                result.append(col)
+                continue
+            str_count = sum(list(map(lambda x: not str(x).isdigit(), df[col].unique())))
+            if str_count <= n:
+                result.append(col)
         return result
     elif method == 2:
         for col in df.columns:
             if (ignore_strings or df[col].dtype in ["float64", "int64"]) and df.shape[
                 0
             ] / 100 < len(df[col].unique()) <= df.shape[0]:
-                result[col] = []
+                result.append(col)
         return result
 
 
-def mark_outliers(series: pd.Series):
-    q1 = series.quantile(0.25)
-    q3 = series.quantile(0.75)
-    iqr = q3 - q1
-    lower_fence = q1 - 1.5 * iqr
-    upper_fence = q3 + 1.5 * iqr
-    # print(~series.apply(lambda x: lower_fence <= x <= upper_fence))
-    return ~series.apply(lambda x: lower_fence <= x <= upper_fence)
+def mark_outliers(series: pd.Series, method: int = 1):
+    """
+    Здесь не используется ignore_string т.к. если он True, то строки и так сохраняются
+    А если он False, то столбцы со строками вообще не должны выбираться, поэтому сюда
+    строковые значения не попадут
+    """
+    if series.dtype not in ['float64', 'int64']:
+        series_without_strings = series[series.apply(lambda x: str(x).isdigit())].astype("float64")
+    if method == 1:
+        q1 = series_without_strings.quantile(0.25)
+        q3 = series_without_strings.quantile(0.75)
+        iqr = q3 - q1
+        lower_fence = q1 - 1.5 * iqr
+        upper_fence = q3 + 1.5 * iqr
+        return ~series.apply(lambda x: (lower_fence <= float(x) <= upper_fence if str(x).isdigit() else True))
+    if method == 2:
+        #here will be some method
+        pass
 
 
 def remove_outliers_from_series(series: pd.Series, method: int = 1):
-    return series[~mark_outliers(series).astype(bool)]
+    return series[~mark_outliers(series, method=method)]
 
 
-def remove_outliers2(df: pd.DataFrame, columns: list[str] = None):
-    pass
-    return df[~sum(mark_outliers(df[col]) for col in columns).astype(bool)]
+def remove_outliers(df: pd.DataFrame, columns: list[str] = None, method: int = 1):
+    return df[~sum(mark_outliers(df[col], method=method) for col in columns).astype(bool)]
 
 
 def print_distr(
@@ -94,19 +98,18 @@ def print_distr(
 ) -> tuple[bool, str]:
     try:
         _, axes = plt.subplots(
-            (len(cols.keys()) // 2) + (1 if len(cols.keys()) % 2 > 0 else 0),
+            (len(cols) // 2) + (1 if len(cols) % 2 > 0 else 0),
             2,
             figsize=figsize,
         )  # не читайте, не надо, это просто работает, ок?
         i, j, max_i = (
             0,
             0,
-            (len(cols.keys()) // 2) + (1 if len(cols.keys()) % 2 > 0 else 0),
+            (len(cols) // 2) + (1 if len(cols) % 2 > 0 else 0),
         )
-        for col in cols.keys():
-            exceptions = cols[col]
+        for col in cols:
             col_of_nums = df[col].apply(
-                lambda x: (-1000 if (x in exceptions or x != x) else float(x))
+                lambda x: (-1000 if (not str(x).isdigit() or x != x) else float(x))
             )  # x != x only when x is NaN
             axes[i, j].hist(col_of_nums, bins=bins)
             axes[i, j].set_xlabel(f"Значение переменной {col}")
@@ -128,14 +131,13 @@ def print_distr(
 
 def is_el_ok(
     elem: str,
-    exceps: list[str],
     Q3: int,
     Q1: int,
-    value_for_exceptions=True,
+    value_for_str=True,
     value_for_nan=True,
 ):
-    if elem in exceps:
-        return value_for_exceptions
+    if not elem.isdigit():
+        return value_for_str
     elem = float(elem)
     IQR = Q3 - Q1
     lower_bound = Q1 - 1.5 * IQR
@@ -147,74 +149,67 @@ def is_el_ok(
     return False
 
 
-def remove_outliers(
+def data_preprocessing(
     df: pd.DataFrame,
     n: int = 2,
     logging: Loggs = Loggs(),
     select_method: int = 1,
     delete_method: int = 1,
+    save_deleted: bool = True,
     ignore_strings: bool = True,
     exclude: list[str] = ["id"],
 ):
     df = test_df(df, logging=logging.test)
 
-    cols = get_dict_of_cols(
+    cols = get_list_of_cols(
         df=df, n=n, exclude=exclude, method=select_method, ignore_strings=ignore_strings
     )
 
-    tprint("Cols:")
-    print()
-    print(*cols, sep=" | ")
-    tprint("============================")
+    if logging.cols_selection:
+        tprint("Selected cols:")
+        print()
+        print(*cols, sep=" | ")
+        tprint("__________")
 
     clear_df = df.copy(deep=True)
     deleted = {}
     """
-    deleted = {
+    deleted = {  # not empty only if save_deleted is True
         method: "iqr" or smthg else
         column: {
             count: (count of deleted elements)
-            deleted: []
+            deleted: {
+                id: value,
+                id: value,
+                ...
+            }
         },
         ...
     }
     """
-    for col in cols.keys():
-        exceptions = cols[col]
-        Q3 = (
-            clear_df[clear_df[col].apply(lambda x: x not in exceptions)][col]
-            .astype(float)
-            .quantile(0.75)
-        )
-        Q1 = (
-            clear_df[clear_df[col].apply(lambda x: x not in exceptions)][col]
-            .astype(float)
-            .quantile(0.25)
-        )
-        values = clear_df[col].apply(
-            lambda x: is_el_ok(elem=str(x), exceps=exceptions, Q3=Q3, Q1=Q1)
-        )
-        if logging:
+    for col in cols:
+        marks = mark_outliers(clear_df[col], method=delete_method)
+        if save_deleted:
             deleted[col] = {}
-            deleted[col]["count"] = (
-                values.value_counts().values[1] if len(values.value_counts()) > 1 else 0
-            )
-            deleted[col]["deleted"] = list(clear_df[~values][col].values)
-            print(
-                f"column: {col} => deleted: {values.value_counts().values[1] if len(values.value_counts().values) > 1 else 0}"
-            )
-        clear_df = clear_df[values]
-    tprint("WAS:")
-    print_distr(df, cols, (10, 13))
-    tprint("============================")
-    tprint("BECAME:")
-    print_distr(clear_df, cols, (10, 13))
-    if logging:
-        return clear_df, deleted
-    return clear_df, {}
+            deleted[col]["count"] = (sum(marks))
+            deleted[col]["deleted"] = dict(zip(list(clear_df[marks][col].keys()), list(clear_df[marks][col].values)))
+            print(f"column: {col} => deleted: {deleted[col]['count']}")
+        clear_df = clear_df[~marks]
+    if logging.was_became:
+        tprint("WAS:")
+        print_distr(df, cols, (10, 13))
+        tprint("__________")
+        tprint("BECAME:")
+        print_distr(clear_df, cols, (10, 13))
+    return clear_df, deleted
 
 
 def test_df(df: pd.DataFrame, autofix=1, logging=False):
+    """
+    Герман попросил
+    Идея в чём, в датасете могут быть столбцы типа object но при этом в них только числа
+    Эта фанка это находит и меняет тип столбца
+    """
     testing = df.select_dtypes(include="object")
     bad_columns = []
     for col in testing.columns:
@@ -227,3 +222,4 @@ def test_df(df: pd.DataFrame, autofix=1, logging=False):
             df[col] = df[col].astype(float)
 
     return df
+
