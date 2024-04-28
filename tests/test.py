d1d5653cc7f339e88aa92963434256e3259bbd92
Dimidrosh <dimidrosh@gmail.com>
2024-04-28 12:07:05 +0300

    changed name from "spandas" to "SPandas"

--- tests/test.py
+++ tests/test.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
-from spandas.preprocessing import *
+from SPandas.preprocessing import *
 
 # url = "ecology.xlsx"
 # url = 'База данных Оригинал.xlsx'
